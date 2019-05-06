f37eed9bd81e5e1965c346f6073942fe832aa620
Mark Hernandez <m.hern1415@gmail.com>
2019-05-06 01:35:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,8 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def summon_captain_planet(planeteer_calls)
+  
 
 def long_planeteer_calls# code an argument here
   # Your code here
