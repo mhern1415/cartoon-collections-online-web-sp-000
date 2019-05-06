25930efeaf67da49dfe3c6a487ebf6e191b8e693
Mark Hernandez <m.hern1415@gmail.com>
2019-05-06 02:02:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(calls)
   end
 end  
 
-def find_the_cheese2(array, array2)
-  cheese_types = array2
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
     cheese_types.include?(type)
   end 
