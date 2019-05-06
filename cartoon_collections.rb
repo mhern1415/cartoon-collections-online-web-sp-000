796da606e4c0c7befdb9adb7e2efa4a8b16134fc
Mark Hernandez <m.hern1415@gmail.com>
2019-05-06 01:33:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,9 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |name, index|
-    index += 1
-    puts "#{index}. #{name}"
-  end
-end
 
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do 
-  # Your code here
+  dwarves.each_with_index do |name, index|
+    index += 1 
+    puts "#{index}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
